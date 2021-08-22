04332569c5788ac721e5440d8bebbb9dc83c9898
Kuro-Mani <37373258+Kuro-Mani@users.noreply.github.com>
2021-08-22 15:06:05 +0900

    Update exping.bat

--- exping.bat
+++ exping.bat
@@ -1,4 +1,4 @@
 @echo off
 for /f %%i IN (D:\ping.txt) DO ping %%i -n 1 >>exping.txt
-for /f %%i IN (D:\ping.txt) DO systeminfo /s %%i >systeminfo.txt
+for /f %%i IN (D:\ping.txt) DO systeminfo /s %%i >%%i.txt
 pause

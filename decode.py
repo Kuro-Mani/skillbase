2570bc1f894eafb206c4c507f53b3b12770b9738
Kuro-Mani <37373258+Kuro-Mani@users.noreply.github.com>
2021-11-27 17:52:35 +0900

    RSAset

---
+++ decode.py
@@ -0,0 +1,11 @@
+from Crypto.Util.number import *
+
+c = int(input('c: '))
+e = int(input('e: '))
+p = int(input('p: '))
+q = int(input('q: '))
+n = p * q
+phi_n = (p-1) * (q-1)
+d = pow(e, -1,phi_n)
+m = pow(c,d,n)
+print(long_to_bytes(m))

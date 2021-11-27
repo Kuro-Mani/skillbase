21da5ed76243c337b0e3cd197653e0e706a2c9a4
Kuro-Mani <37373258+Kuro-Mani@users.noreply.github.com>
2021-11-27 16:05:50 +0900

    Add files via upload

---
+++ calc_stream.py
@@ -0,0 +1,17 @@
+import socket
+import re
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.100.31', 12303))
+while True:
+    receivedata = s.recv(1024).decode('utf-8')
+    print("[+]receivedata=",receivedata)
+    splitreceivedata = receivedata.split("\n")
+    formula = splitreceivedata[len(splitreceivedata)-1]
+    pattern = '(.*) ='
+    m = re.search(pattern, formula)
+    if m:
+        formula = m.group(1)
+        print("[+]formula=",formula)
+    senddata = str(eval(formula))
+    print("[+]senddata=",senddata)
+    s.sendall((senddata+"\n").encode("utf-8"))
